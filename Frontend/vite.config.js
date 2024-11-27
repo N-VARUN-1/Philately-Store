3c9ac5499e1147e2f68b3573f42be0c9192174f6
NANDURI VARUN <159157923+N-VARUN-1@users.noreply.github.com>
2024-11-28 01:51:38 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server:{
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://philately-store-backendnew.vercel.app',
         secure: true
       },
     },
