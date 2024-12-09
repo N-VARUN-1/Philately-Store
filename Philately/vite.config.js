cc6507e3cfa74a33f63cd40be09143752374b3a9
NANDURI VARUN <159157923+N-VARUN-1@users.noreply.github.com>
2024-12-09 11:48:26 +0530

    Update vite.config.js

--- Philately/vite.config.js
+++ Philately/vite.config.js
@@ -3,15 +3,16 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // server:{
-  //   proxy: {
-  //     '/api/(.*)': {
-  //       target: 'https://philately-store-backend-ne-1.vercel.app',
-  //       changeOrigin: true,
-  //       secure: true,
-  //     },
-  //   },
-  // },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://philately-store-backend-ne-1.vercel.app',
+        changeOrigin: true,
+        secure: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
   plugins: [react()],
   base: '/',
   build: {
