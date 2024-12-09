9002184a8e2460d3f5366c527eb15f3277f39ff4
NANDURI VARUN <159157923+N-VARUN-1@users.noreply.github.com>
2024-12-09 12:02:17 +0530

    Update index.cjs

--- api/index.cjs
+++ api/index.cjs
@@ -36,14 +36,6 @@ app.use(cors({
   credentials: true
 }));
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://philately-store-frontend.vercel.app');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Allow-Credentials', 'true'); // For cookies or authentication
-    next();
-});
-
 
 // Explicit OPTIONS handler
 app.options('*', cors());
