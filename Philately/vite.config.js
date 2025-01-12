e2c3125bbbc190336cc5c65794339b077df219de
NANDURI VARUN <159157923+N-VARUN-1@users.noreply.github.com>
2025-01-12 23:40:04 +0530

    Update vite.config.js

--- Philately/vite.config.js
+++ Philately/vite.config.js
@@ -1,12 +1,31 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
+  
+  // Render.com specific configurations
+  base: '/',
+  
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  },
+  
   server: {
     proxy: {
       '/api': {
-        target: 'https://philately-store-1.onrender.com',
+        target: 'https://your-backend-url.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
