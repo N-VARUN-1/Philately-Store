dc0f248c7eacaa835d0f35737760bc31cab096d7
NANDURI VARUN <159157923+N-VARUN-1@users.noreply.github.com>
2025-01-12 20:01:53 +0530

    Delete Philately directory

--- Philately/src/Pages/ExplorePage/CollectorsPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function CollectorsPage() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
--- Philately/src/Pages/ExplorePage/MintstampsPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function MintstampsPage() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
