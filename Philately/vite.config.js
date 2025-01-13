ce9888e527d12f0ab2d8092d7f872687ce6588fd
NANDURI VARUN <159157923+N-VARUN-1@users.noreply.github.com>
2025-01-14 00:54:49 +0530

    Update vite.config.js

--- Philately/vite.config.js
+++ Philately/vite.config.js
@@ -6,8 +6,6 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        port: 10000,
-        host: '0.0.0.0',
         target: 'https://philately-store-1.onrender.com',
         changeOrigin: true, // Ensure the origin header matches the target server
         secure: true,       // For https targets
