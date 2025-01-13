40792213bc9663e798a734d8465b25cd39a4473b
NANDURI VARUN <159157923+N-VARUN-1@users.noreply.github.com>
2025-01-14 01:29:17 +0530

    Update vite.config.js

--- Philately/vite.config.js
+++ Philately/vite.config.js
@@ -3,6 +3,17 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0', // Bind to all network interfaces
+    port: 10000 // Use PORT or fallback to 3000
+    proxy: {
+      '/api': {
+        target: 'https://philately-store-1.onrender.com',
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
   plugins: [react()],
   build: {
     outDir: 'dist' // Output directory for production build
