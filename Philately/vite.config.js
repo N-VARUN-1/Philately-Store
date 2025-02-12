239f45bee4686ef0e6045dc97cac143f81a400fb
NANDURI VARUN <159157923+N-VARUN-1@users.noreply.github.com>
2025-02-13 01:35:22 +0530

    Update vite.config.js

--- Philately/vite.config.js
+++ Philately/vite.config.js
@@ -4,8 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    host: '0.0.0.0', // Bind to all network interfaces
-    // port: 10000, // Use PORT or fallback to 3000
     proxy: {
       '/api': {
         target: process.env.API,
@@ -14,8 +12,5 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react()],
-  build: {
-    outDir: 'dist' // Output directory for production build
-  }
+  plugins: [react()]
 })
